64feae988762a1ab4a9fcfa16cccacb94339a2a7
sarangrumah <sarangrumah.dev@gmail.com>
2025-09-03 15:27:55 +0700

    toaster

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@
  * "@/components/ui/toaster" does not exist.
  * If you want to use a Toaster, use the Sonner import below.
  */
-// import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
+// import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -38,8 +38,8 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      {/* <Toaster /> */}
-      <Sonner />
+      <Toaster />
+      {/* <Sonner /> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
