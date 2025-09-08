f5479dd85345741beac43313f615860743c70aa3
sarangrumah <sarangrumah.dev@gmail.com>
2025-09-08 08:01:30 +0700

    console

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,5 @@ import App from './App.tsx'
 import './index.css'
 import './i18n/index.ts'
 console.log('VITE_API_URL:', import.meta.env.VITE_API_URL);
+console.log(window.location.href);
 createRoot(document.getElementById("root")!).render(<App />);
