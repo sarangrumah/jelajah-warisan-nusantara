7881ba74e570849456ca08d61dd2ada7a7d600c8
sarangrumah <sarangrumah.dev@gmail.com>
2025-09-08 07:31:20 +0700

    add console

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './i18n/index.ts'
-
+console.log('VITE_API_URL:', import.meta.env.VITE_API_URL);
 createRoot(document.getElementById("root")!).render(<App />);
