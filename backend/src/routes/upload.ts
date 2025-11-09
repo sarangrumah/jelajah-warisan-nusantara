6503f85b79c43e9292cf369544c136de9b37e2af
Ade Maryadi <sarangrumah.dev@gmail.com>
2025-11-10 02:39:07 +0700

    upload issues

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -46,7 +46,8 @@ const bucketWhitelist = [
   'sites',
   'events',
   'museum',
-  'memory-thumbnails'
+  'memory-thumbnails',
+  'publication'
 ];
 
 const allowedBuckets = new Set(bucketWhitelist);
